6f7fbd5e1035d86019e12047d1ddfde4b3830a5b
Rodrigo Barreiro <rodrigoasbarreiro@gmail.com>
2025-08-14 21:24:09 -0300

    Sequence Length Distribution

---
+++ R/plot_sequence_length_distribution.R
@@ -0,0 +1,53 @@
+#' FastQC-viz: Sequence Length Distribution
+#'
+#' @description
+#' Create plot for "Sequence Length Distribution"
+#'
+#' @details
+#' Create plot for "Sequence Length Distribution"
+#'
+#' @param NULL
+#'
+#' @return ggplot
+#'
+#' @keywords plot
+#'
+#' @examples
+#' plot_sequence_length_distribution(fastqc_data)
+#'
+#' @export
+plot_sequence_length_distribution <- function(fastqc_data) {
+  fqcviz_colors <- get_color_palette()
+
+  data2plot <-
+    fastqc_data$sequence_length_distribution$content |>
+    dplyr::transmute(
+      length = as.numeric(length),
+      count = as.numeric(count)
+    )
+
+  data2plot |>
+    ggplot2::ggplot(ggplot2::aes(x = length, y = count)) +
+    ggplot2::geom_col(fill = fqcviz_colors$blue1) +
+    ggplot2::labs(
+      x = "Sequence Length (bp)",
+      y = "Sequences (N)",
+      color = NULL
+    ) +
+    ggplot2::scale_y_continuous(
+      label = function(x) {
+        format_large_numbers(x, digits = 1)
+      },
+      expand = c(0, 0)
+    ) +
+    ggplot2::theme(
+      legend.position = c(1, 1),
+      legend.justification = c(1, 1),
+      legend.key.spacing.y = unit(.1, "cm"),
+      legend.key.height = unit(.1, "cm")
+    ) +
+    ggplot2::scale_x_continuous(
+      expand = ggplot2::expansion(add = 3),
+      breaks = seq(min(data2plot$length), max(data2plot$length), 1)
+    )
+}
